ce65f9be13a8f6b141922c2b407b12e29c905dbf
gzeinnumer <gzeinnumer@github.com>
2018-12-15 23:02:35 +0700

    master

---
+++ app/src/main/java/com/zein/listview_costuminnerclases/MainActivity.java
@@ -0,0 +1,68 @@
+package com.zein.listview_costuminnerclases;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] IMAGES = {
+            R.drawable.gambar_1, R.drawable.gambar_1, R.drawable.gambar_1,
+            R.drawable.gambar_1, R.drawable.gambar_1, R.drawable.gambar_1,
+            R.drawable.gambar_1, R.drawable.gambar_1, R.drawable.gambar_1,
+            R.drawable.gambar_1};
+
+    String[] NAMES = {"Abi","Bebi","Caca","Deri","Elin","Fadil","Gina","Haris","Intan","Juna"};
+
+    String[] DESCRIPTION = {"Student","Actor","Artis","Employee","Driver","Pilice","Security","Reseptionis","Sports","Driver"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        CostumAdapter costumAdapter= new CostumAdapter();
+        listView.setAdapter(costumAdapter);
+
+    }
+
+    class CostumAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return IMAGES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.costum_layout,null);
+
+            ImageView imageView = convertView.findViewById(R.id.imageView);
+            TextView textView_name = convertView.findViewById(R.id.textView_name);
+            TextView textView_description = convertView.findViewById(R.id.textView_desciption);
+
+            imageView.setImageResource(IMAGES[position]);
+            textView_name.setText(NAMES[position]);
+            textView_description.setText(DESCRIPTION[position]);
+
+            return convertView;
+        }
+    }
+}
